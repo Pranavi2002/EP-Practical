d9cc52bfb963687d28cfa78e5df0bb44dcfaa234
Pranavi2002 <66314704+Pranavi2002@users.noreply.github.com>
2021-01-18 10:59:36 +0530

    Task-3 StudentBean class

---
+++ StudentBean.java
@@ -0,0 +1,24 @@
+package com.klu.Student;
+
+public class StudentBean {
+	private int regno;
+	private String name,email;
+	public void setRegno(int regno) {
+		this.regno = regno;
+	}
+	public int getRegno() {
+		return regno;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail() {
+		return email;
+	}
+}
