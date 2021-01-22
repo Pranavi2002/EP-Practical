551200af3943988f6b084ef8126cea27d33d985c
Pranavi2002 <66314704+Pranavi2002@users.noreply.github.com>
2021-01-22 09:55:57 +0530

    Task-3 DBUtil Class

--- DBUtil.java
+++ DBUtil.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-	final static String forNameURL = "com.mysql.jdbc.Driver";
-	final static String dBURL = "jdbc:mysql:thin:@localhost:3306:EP";
+	final static String forNameURL = "com.mysql.cj.jdbc.Driver";
+	final static String dBURL = "jdbc:mysql://localhost:3306/EP";
 	final static String username = "root";
 	final static String password = "root";
 	public static Connection DBConnection() throws ClassNotFoundException, SQLException{
